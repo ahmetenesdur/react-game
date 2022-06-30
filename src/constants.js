8944fe9c7a3abb58cee76ab44d7a2d8099c22d40
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-06-30 18:33:37 +0300

    first commit

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+const CONTRACT_ADDRESS = '0xb0748A7372E15439B7c77dd7d98036BD28d4C4b6';
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+
+export { CONTRACT_ADDRESS,transformCharacterData };
